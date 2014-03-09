ff29f220f50e78bb223af2a3ccc2ee399114ebdd
sudongsheng <sudongsheng123@gmail.com>
2014-03-09 17:09:18 +0800

    init the setting model

--- src/org/nupter/mmdaily/ui/SettingActivity.java
+++ src/org/nupter/mmdaily/ui/SettingActivity.java
@@ -1,62 +1,149 @@
 package org.nupter.mmdaily.ui;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.SimpleAdapter;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 import org.nupter.mmdaily.R;
-import org.nupter.mmdaily.utils.CornerListView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by sudongsheng on 14-3-1.
  */
-public class SettingActivity extends Activity{
+public class SettingActivity extends Activity {
 
-    private CornerListView otherListView;
+    private ToggleButton autoDownload;
+    private ToggleButton nightMode;
+    private ToggleButton noPicMode;
+    private ToggleButton pushMessage;
+    private TextView download;
+    private TextView textSize;
+    private LinearLayout clear;
+    private LinearLayout share;
+    private LinearLayout suggestion;
+    private LinearLayout update;
+    private LinearLayout about;
 
-    private List<Map<String, String>> listData = null;
-    private SimpleAdapter simpleAdapter = null;
-    private Adapter adapter=null;
+    private SharedPreferences mySharedPreferences;
+
+    private boolean autoDownload_flag;
+    private boolean nightMode_flag;
+    private boolean noPicMode_flag;
+    private boolean pushMessage_flag;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        otherListView = (CornerListView) findViewById(R.id.other);
-        setListData();
-        simpleAdapter = new SimpleAdapter(SettingActivity.this, listData, R.layout.view_other_adapter,
-                new String[]{"text"}, new int[]{R.id.other_textView});
-        otherListView.setAdapter(simpleAdapter);
+        mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        findViewByIds();
+        setOnClickListeners();
+        autoDownload_flag = mySharedPreferences.getBoolean("autoDownload", false);
+        nightMode_flag = mySharedPreferences.getBoolean("nightMode", false);
+        noPicMode_flag = mySharedPreferences.getBoolean("noPicMode", false);
+        pushMessage_flag = mySharedPreferences.getBoolean("pushMessage", false);
+        autoDownload.setChecked(autoDownload_flag);
+        nightMode.setChecked(nightMode_flag);
+        noPicMode.setChecked(noPicMode_flag);
+        pushMessage.setChecked(pushMessage_flag);
     }
 
-    private void setListData() {
-        listData = new ArrayList<Map<String, String>>();
-
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("text", "分享");
-        listData.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("text", "清除缓存");
-        listData.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("text", "意见反馈");
-        listData.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("text", "检查更新");
-        listData.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("text", "关于");
-        listData.add(map);
-
+    public void findViewByIds() {
+        autoDownload = (ToggleButton) findViewById(R.id.autoDownload);
+        nightMode = (ToggleButton) findViewById(R.id.nightMode);
+        noPicMode = (ToggleButton) findViewById(R.id.noPicMode);
+        pushMessage = (ToggleButton) findViewById(R.id.pushMessage);
+        download = (TextView) findViewById(R.id.download);
+        textSize = (TextView) findViewById(R.id.textSize);
+        share = (LinearLayout) findViewById(R.id.share);
+        clear = (LinearLayout) findViewById(R.id.clear);
+        suggestion = (LinearLayout) findViewById(R.id.suggestion);
+        update = (LinearLayout) findViewById(R.id.update);
+        about = (LinearLayout) findViewById(R.id.about);
     }
 
+    public void setOnClickListeners() {
+        autoDownload.setOnCheckedChangeListener(checkedListener);
+        nightMode.setOnCheckedChangeListener(checkedListener);
+        noPicMode.setOnCheckedChangeListener(checkedListener);
+        pushMessage.setOnCheckedChangeListener(checkedListener);
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        textSize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        suggestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    CompoundButton.OnCheckedChangeListener checkedListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            SharedPreferences.Editor editor = mySharedPreferences.edit();
+            if (buttonView.getId() == R.id.autoDownload) {
+                if (isChecked)
+                    autoDownload_flag = true;
+                else
+                    autoDownload_flag = false;
+                editor.putBoolean("autoDownload", autoDownload_flag);
+            } else if (buttonView.getId() == R.id.nightMode) {
+                if (isChecked)
+                    nightMode_flag = true;
+                else
+                    nightMode_flag = false;
+                editor.putBoolean("nightMode", nightMode_flag);
+            } else if (buttonView.getId() == R.id.noPicMode) {
+                if (isChecked)
+                    noPicMode_flag = true;
+                else
+                    noPicMode_flag = false;
+                editor.putBoolean("noPicMode", noPicMode_flag);
+            } else if (buttonView.getId() == R.id.pushMessage) {
+                if (isChecked)
+                    pushMessage_flag = true;
+                else
+                    pushMessage_flag = false;
+                editor.putBoolean("pushMessage", pushMessage_flag);
+            }
+            editor.commit();
+        }
+    };
 }
